fe9bafe1e3374d86c4897eb05fdf722386610361
Afiz Savage <savageafiz9@gmail.com>
2022-01-04 16:54:36 +0000

    Hit the api endpoint

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,5 +1,31 @@
 import '../style.css';
-import Logo from '../assets/A & S-logos_transparent.png'
+import Logo from '../assets/A & S-logos_transparent.png';
 
 const logo = document.querySelector('img');
-logo.src = Logo
+logo.src = Logo;
+
+const fetchMeals = async () => {
+  const response = await fetch(
+    'https://themealdb.com/api/json/v1/1/search.php?f=a'
+  );
+  const meals = await response.json();
+  return meals;
+};
+
+const displayMeals = (meals) => {
+  const listContainer = document.getElementById('items-list');
+
+  let listItems = '';
+  meals.meals.forEach((meal) => {
+    listItems += `<li>${meal.strMeal}</li>`;
+    listContainer.innerHTML = listItems;
+  });
+};
+
+const load = () => {
+  fetchMeals().then((meals) => {
+    displayMeals(meals);
+  });
+};
+
+window.onload = load;
