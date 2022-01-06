249283393332c27cecb35d5b06feed737630ddf0
kingqabil <kingqabil@live.com>
2022-01-06 09:29:22 +0300

    Added popup modal

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -3,7 +3,7 @@ import Logo from '../assets/A & S-logos_transparent.png';
 
 const logo = document.querySelector('img');
 logo.src = Logo;
-
+const mainSection = document.querySelector('main');
 const fetchMeals = async () => {
   const response = await fetch(
     'https://themealdb.com/api/json/v1/1/search.php?f=m'
@@ -11,6 +11,41 @@ const fetchMeals = async () => {
   const meals = await response.json();
   return meals;
 };
+
+const createDetailsPopup = (object) => {
+  //body.classList.add('disScroll');
+  const modal = document.createElement('div');
+  modal.id = 'modal';
+  const modalContent = `  <article>
+    <h3>${object.idMeal}</h3>
+    <p></p>
+    <footer>
+      <div>
+        <button>See live</button>
+        <button>See Source</button>
+      </div>
+      <div id="navigate">
+        <button>Previous project</button>
+        <button>Next project</button>
+      </div>
+    </footer>
+  </article>`;
+  modal.innerHTML = modalContent;
+  mainSection.appendChild(modal);
+  /* const closeButton = document.getElementById('modal-icon'); */
+  /* closeButton.addEventListener('click', () => {
+    body.classList.remove('disScroll');
+    modal.remove();
+  }); */
+};
+
+function addEvtListenerToButtons(buttons, projectObject) {
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      createDetailsPopup(projectObject);
+    });
+  });
+}
 
 const displayMeals = (meals) => {
   const listContainer = document.getElementById('items-list');
@@ -22,12 +57,8 @@ const displayMeals = (meals) => {
     <div class='card-header'><span class='meal-name'>${meal.strMeal}</span> <span><button class='like-icon' type='button'></button></span></div>
     <div class='cmtbtn-sec'><button type='button'>Comments</button></div> </div></li>`;
     listContainer.innerHTML = listItems;
-  });
-  buttons = document.querySelectorAll('.cmtbtn-sec > button');
-  buttons.forEach((button) =>{
-    button.addEventListener('click',() => {
-      console.log('click');
-    });
+    buttons = document.querySelectorAll('.cmtbtn-sec > button');
+    addEvtListenerToButtons(buttons, meal);
   });
 };
 
@@ -38,5 +69,3 @@ const load = () => {
 };
 
 window.onload = load;
-
-const popup = document.getElementById(popup);
